5974a7b9488456e2160abb6b5a2d1430b37006a0
Suryansh Mathur <101623861+suryanshqs@users.noreply.github.com>
2023-10-25 01:47:33 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,124 @@
-import React, { useState } from 'react';
-import './App.css';
-import { clear } from '@testing-library/user-event/dist/clear';
+// Very first React project :D
 
+import "./App.css";
+import { useState } from "react";
+// useState is a React hook that takes a state's initial value and returns an array with 2 values,
+// i.e., a getter and setter function
 
-const App = () =>{
-  const [result, setResult] = useState("");
+import { MdAdd } from "react-icons/md";
+import { SlTrash } from "react-icons/sl";
+import { VscGithub } from "react-icons/vsc";
+import {SiLinkedin } from "react-icons/si";
 
-  const handleclick = (e) =>{
-    setResult(result.concat (e.target.name))
-
-  }
-  const clear = () =>{
-     setResult("");
-  }
-
-  const backspace = () =>{
-       setResult(result.slice(0, - 1))
-  }
-
-  const calculate = () =>{
-    setResult(eval(result).toString())
-  }
-  return(
-    <>
-    <div className ="container">
-      <form>
-        <input type="text" value={result}/>
-      </form>
-
-    <div className="keypad">
-        <button className = "highlight"onClick={clear} id = "clear">Clear</button>
-        <button className = "highlight" onClick={backspace} id="backspace">C</button>
-        <button className = "highlight" name="/" onClick={handleclick}>&divide;</button>
-        <button name="7" onClick={handleclick}>7</button>
-        <button name="8"  onClick={handleclick}>8</button>
-        <button name="9"  onClick={handleclick}>9</button>
-        <button className = "highlight" name="*"  onClick={handleclick}>&times;</button>
-        <button name="4"  onClick={handleclick}>4</button>
-        <button name="5"  onClick={handleclick}>5</button>
-        <button name="6"  onClick={handleclick}>6</button>
-        <button className = "highlight" name="-"  onClick={handleclick}>&ndash;</button>
-        <button name="1"  onClick={handleclick}>1</button>
-        <button name="2"  onClick={handleclick}>2</button>
-        <button name="3"  onClick={handleclick}>3</button>
-        <button className = "highlight" name="+"  onClick={handleclick}>+</button>
-        <button name="0"  onClick={handleclick}>0</button>
-        <button name="."  onClick={handleclick}>.</button>
-        <button  className = "highlight"onClick={calculate} id = "result">=</button>
-
+const TodoInput = ({ todo, setTodo, addTodo, setTodos, todos }) => {
+  return (
+    <div className="input-wrapper">
+      <input
+        type="text"
+        id="todo-input"
+        name="todo"
+        value={todo}
+        placeholder="Let's get workin'..."
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+        onKeyUp={(e) => {
+          if(e.key==='Enter') {
+            e.preventDefault();
+            setTodo(e.target.value);
+            setTodos([...todos, todo]);
+            setTodo('');
+          }
+        }}
+      ></input>
+      <button className="add-button" id="urmom" onClick={addTodo}>
+        <MdAdd size={21}/>
+      </button>
     </div>
+  );
+};
 
-      </div>
+const TodoList = ({ todoList, removeTodo }) => {
+  return (
+    <div className="input-list">
+      {todoList?.length > 0 ? (
+        <ul className="todo-list">
+          {todoList.map((entry, index) => (
+            <div className="todo">
+              <li key={index}>{entry}</li>
+              <button className="delete-button" onClick={()=>{removeTodo(entry)}}>
+                <SlTrash size={18}/>
+
+              </button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <p>Add some tasks! :)</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <>
+        <p className="love">
+        Made with 💖
+        </p>
+        <p className="socials"> 
+          <a href="https://github.com/adelicia-js" rel="noreferrer" target="_blank"><VscGithub size={25}/></a>
+          <a href="https://www.linkedin.com/in/adelicia/" rel="noreferrer" target="_blank"><SiLinkedin size={25}/></a>
+        </p>
+        <p>
+            <a href="https://github.com/adelicia-js/todo-cra" rel="noreferrer" target="_blank" className="source">$Source | 2023 - 2024</a> 
+        </p> 
+        
     </>
   );
-  }
+};
 
-export default App
+const App = () => {
+  // Create a todo
+  const [todo, setTodo] = useState("");
+  // Getter function: todo -> displays current state
+  // In this case, initial value is undefined, i.e., ""
+  // Setter function: setTodo -> sets/updates state
+
+  // Add a todo
+  const [todos, setTodos] = useState([]);
+  // Getter function: todos -> empty array (todos are pushed to it)
+  // Setter function: setTodos -> updates todos array using addTodo()
+
+  // Function to add todo to array
+  const addTodo = () => {
+    if (todo !== "") {
+      // Ensures that input isn't empty
+      setTodos([...todos, todo]);
+      setTodo(""); // Clears input after todo is pushed to array
+    }
+  };
+  // todo is pushed to todos (a copy of todos using ... operator)
+
+  const deleteTodo = (task) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== task;
+    });
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>Make a To-Do List!</h1>
+      <div className="Content">
+        <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} setTodos={setTodos} todos={todos}/>
+        <TodoList todoList={todos} removeTodo={deleteTodo} />
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
